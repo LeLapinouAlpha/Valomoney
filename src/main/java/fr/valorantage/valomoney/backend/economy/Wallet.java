0bce0fe99b4c61f7afe8a65f4a8d9dece3b6eb00
Théo Hénon <t-henon@hotmail.fr>
2024-03-07 18:20:39 +0100

    feat(economy): create Wallet class and implement first constructor

---
+++ src/main/java/fr/valorantage/valomoney/backend/economy/Wallet.java
@@ -0,0 +1,35 @@
+package fr.valorantage.valomoney.backend.economy;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.UUID;
+
+public final class Wallet {
+    private final UUID playerId;
+    private float balance;
+
+    public Wallet(UUID playerId) {
+        this.playerId = playerId;
+        balance = 0.0f;
+    }
+
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(float amount) {
+        throw new NotImplementedException();
+    }
+
+    public void addMoney(float amount) {
+        throw new NotImplementedException();
+    }
+}
