8076b83d5bcf54323d33cdfd08cf79b21866e3cb
Théo Hénon <t-henon@hotmail.fr>
2024-03-08 19:34:43 +0100

    feat(ValomoneyMd): add bill items

--- src/main/java/fr/valorantage/valomoney/ValomoneyMod.java
+++ src/main/java/fr/valorantage/valomoney/ValomoneyMod.java
@@ -2,6 +2,12 @@ package fr.valorantage.valomoney;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -11,6 +17,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -22,12 +31,39 @@ public class ValomoneyMod
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final RegistryObject<Item> BILL_FIVE = ITEMS.register("bill_five", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BILL_TEN = ITEMS.register("bill_ten", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BILL_TWENTY = ITEMS.register("bill_twenty", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BILL_FIFTY = ITEMS.register("bill_fifty", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BILL_HUNDRED = ITEMS.register("bill_hundred", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<CreativeModeTab> VALOMONEY_TAB = CREATIVE_MODE_TABS.register("valomoney_tab", () -> CreativeModeTab.builder()
+            .title(Component.literal("Valomoney"))
+            .icon(() -> BILL_FIVE.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(BILL_FIVE.get());
+                output.accept(BILL_TEN.get());
+                output.accept(BILL_TWENTY.get());
+                output.accept(BILL_FIFTY.get());
+                output.accept(BILL_HUNDRED.get());
+            }).build());
+
     public ValomoneyMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
+
+        // Register the Deferred Register to the mod event bus so blocks get registered
+        BLOCKS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so items get registered
+        ITEMS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so tabs get registered
+        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
