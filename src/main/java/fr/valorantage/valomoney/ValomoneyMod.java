378cdd48b4305dc40bf6d41598a771141f802696
Théo Hénon <t-henon@hotmail.fr>
2024-03-09 14:03:36 +0100

    refactor(ValomoneyMod): move ITEMS register in ItemsRegister class of "items" subpackage

--- src/main/java/fr/valorantage/valomoney/ValomoneyMod.java
+++ src/main/java/fr/valorantage/valomoney/ValomoneyMod.java
@@ -1,6 +1,7 @@
 package fr.valorantage.valomoney;
 
 import com.mojang.logging.LogUtils;
+import fr.valorantage.valomoney.items.ItemsRegister;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -29,23 +30,17 @@ public class ValomoneyMod
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    public static final RegistryObject<Item> BILL_FIVE = ITEMS.register("bill_five", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> BILL_TEN = ITEMS.register("bill_ten", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> BILL_TWENTY = ITEMS.register("bill_twenty", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> BILL_FIFTY = ITEMS.register("bill_fifty", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> BILL_HUNDRED = ITEMS.register("bill_hundred", () -> new Item(new Item.Properties()));
     public static final RegistryObject<CreativeModeTab> VALOMONEY_TAB = CREATIVE_MODE_TABS.register("valomoney_tab", () -> CreativeModeTab.builder()
             .title(Component.literal("Valomoney"))
-            .icon(() -> BILL_FIVE.get().getDefaultInstance())
+            .icon(() -> ItemsRegister.BILL_FIVE.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
-                output.accept(BILL_FIVE.get());
-                output.accept(BILL_TEN.get());
-                output.accept(BILL_TWENTY.get());
-                output.accept(BILL_FIFTY.get());
-                output.accept(BILL_HUNDRED.get());
+                output.accept(ItemsRegister.BILL_FIVE.get());
+                output.accept(ItemsRegister.BILL_TEN.get());
+                output.accept(ItemsRegister.BILL_TWENTY.get());
+                output.accept(ItemsRegister.BILL_FIFTY.get());
+                output.accept(ItemsRegister.BILL_HUNDRED.get());
             }).build());
 
     public ValomoneyMod()
@@ -56,7 +51,7 @@ public class ValomoneyMod
         modEventBus.addListener(this::commonSetup);
 
         // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
+        ItemsRegister.ITEMS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so tabs get registered
         CREATIVE_MODE_TABS.register(modEventBus);
 
