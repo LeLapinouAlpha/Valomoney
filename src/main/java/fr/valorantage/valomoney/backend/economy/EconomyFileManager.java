9cd0cc1a8cee3378704d91cff5ea1e405ed1a2b2
Théo Hénon <t-henon@hotmail.fr>
2024-03-12 17:56:45 +0100

    refactor(EconomyFileManager): remove saveWallets & restoreWallets methods (to reimplement later)

--- src/main/java/fr/valorantage/valomoney/backend/economy/EconomyFileManager.java
+++ src/main/java/fr/valorantage/valomoney/backend/economy/EconomyFileManager.java
@@ -1,12 +1,6 @@
 package fr.valorantage.valomoney.backend.economy;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 import java.io.File;
-import java.io.FileFilter;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 
 public final class EconomyFileManager {
     private File directory;
@@ -20,25 +14,5 @@ public final class EconomyFileManager {
             throw new IllegalArgumentException(String.format("%s is not a directory", directoryPath));
 
         this.directory = directory;
-    }
-
-    public void saveWallets(final ArrayList<Wallet> wallets) {
-        wallets.forEach(wallet -> {
-            try {
-                Wallet.save(wallet, Paths.get(directory.getPath(), wallet.getPlayerId().toString() + ".wallet").toString());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    public ArrayList<Wallet> restoreWallets() throws IOException, ClassNotFoundException {
-        var wallets = new ArrayList<Wallet>();
-        var files = directory.listFiles(pathname -> pathname.isFile() && pathname.toString().endsWith(".wallet") && pathname.canRead());
-        if (files != null)
-            for (var file : files)
-                wallets.add(Wallet.restore(file.getPath()));
-
-        return wallets;
     }
 }
