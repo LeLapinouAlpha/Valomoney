7a304df943baad1ac968bf1d6403642a503d0ce5
Théo Hénon <t-henon@hotmail.fr>
2024-03-12 12:32:46 +0100

    feat(commands): add "credit" & "debit" args to "money" custom command

--- src/main/java/fr/valorantage/valomoney/commands/MoneyCommand.java
+++ src/main/java/fr/valorantage/valomoney/commands/MoneyCommand.java
@@ -1,25 +1,52 @@
 package fr.valorantage.valomoney.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.FloatArgumentType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 
-import java.util.Objects;
-
 
 public final class MoneyCommand {
     public MoneyCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(Commands.literal("money").executes((command -> displayPlayerMoney(command.getSource()))));
+        dispatcher.register(
+                Commands.literal("money")
+                        .executes((command -> displayPlayerMoney(command.getSource())))
+                        .then(Commands.literal("credit")
+                                .then(Commands.argument("amount", FloatArgumentType.floatArg())
+                                        .executes((command -> credit(command.getSource(), FloatArgumentType.getFloat(command, "amount")))))
+                        )
+                        .then(Commands.literal("debit")
+                                .then(Commands.argument("amount", FloatArgumentType.floatArg())
+                                        .executes((command -> debit(command.getSource(), FloatArgumentType.getFloat(command, "amount")))))
+                        )
+        );
     }
 
     private static int displayPlayerMoney(CommandSourceStack source) {
         if (source.getPlayer() != null) {
             source.sendSuccess(() -> Component.literal("/money command executed!"), true);
             return 1;
-        }
-        else {
+        } else {
             source.sendFailure(Component.literal("The /money command must be executed by a player!"));
+            return -1;
+        }
+    }
+
+    private static int credit(CommandSourceStack source, float amount) {
+        if (source.getPlayer() != null) {
+            source.sendSuccess(() -> Component.literal("The /money credit <amount> has been executed"), true);
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    private static int debit(CommandSourceStack source, float amount) {
+        if (source.getPlayer() != null) {
+            source.sendSuccess(() -> Component.literal("The /money debit <amount> has been executed"), true);
+            return 1;
+        } else {
             return -1;
         }
     }
