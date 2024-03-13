332a0df2aa8b635394722e1991c72c6d51c28eef
LeLapinouAlpha <t-henon@hotmail.fr>
2024-03-13 17:18:34 +0100

    style(MonetaryItem): append hover text that display unit value & total value of a MonetaryItem stack

--- src/main/java/fr/valorantage/valomoney/items/MonetaryItem.java
+++ src/main/java/fr/valorantage/valomoney/items/MonetaryItem.java
@@ -1,6 +1,13 @@
 package fr.valorantage.valomoney.items;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public abstract class MonetaryItem extends Item {
     private final float unitValue;
@@ -8,5 +15,12 @@ public abstract class MonetaryItem extends Item {
     public MonetaryItem(final float unitValue) {
         super(new Item.Properties());
         this.unitValue = unitValue;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> componentList, TooltipFlag flag) {
+        componentList.add(Component.literal(String.format("Unit value: %.2f$", unitValue)));
+        componentList.add(Component.literal(String.format("Total value: %.2f$", unitValue * stack.getCount())));
+        super.appendHoverText(stack, level, componentList, flag);
     }
 }
