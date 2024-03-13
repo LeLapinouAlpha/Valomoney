54db72792fa48ee5441f25b596294c479eb5b5db
LeLapinouAlpha <t-henon@hotmail.fr>
2024-03-13 16:07:35 +0100

    feat(items): create coins textures, models, lang & items and add them into the creative mode tab

--- src/main/java/fr/valorantage/valomoney/items/ItemsRegister.java
+++ src/main/java/fr/valorantage/valomoney/items/ItemsRegister.java
@@ -15,4 +15,7 @@ public final class ItemsRegister {
     public static final RegistryObject<Item> BILL_TWENTY = ITEMS.register("bill_twenty", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BILL_FIFTY = ITEMS.register("bill_fifty", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BILL_HUNDRED = ITEMS.register("bill_hundred", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COIN_FIFTY = ITEMS.register("coin_fifty", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COIN_ONE = ITEMS.register("coin_one", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COIN_TWO = ITEMS.register("coin_two", () -> new Item(new Item.Properties()));
 }
