d11f6daa3e0745d636ee036157f9a3f5a76e6f2d
LeLapinouAlpha <t-henon@hotmail.fr>
2024-03-24 22:31:05 +0100

    refactor(commands): remove "/shop" command

--- src/main/java/fr/valorantage/valomoney/events/CommandEvents.java
+++ src/main/java/fr/valorantage/valomoney/events/CommandEvents.java
@@ -3,7 +3,6 @@ package fr.valorantage.valomoney.events;
 import fr.valorantage.valomoney.ValomoneyMod;
 import fr.valorantage.valomoney.commands.MoneyCommand;
 import fr.valorantage.valomoney.commands.PayCommand;
-import fr.valorantage.valomoney.commands.ShopCommand;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,7 +14,6 @@ public final class CommandEvents {
     public static void onCommandsRegister(RegisterCommandsEvent event) {
         new MoneyCommand(event.getDispatcher());
         new PayCommand(event.getDispatcher());
-        new ShopCommand(event.getDispatcher());
         ConfigCommand.register(event.getDispatcher());
     }
 }
