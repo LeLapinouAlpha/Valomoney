6e7417fd3bf82b1272b33d7c395cae4497a3cc51
LeLapinouAlpha <t-henon@hotmail.fr>
2025-04-28 20:55:40 +0200

    feat: monetaryItems (#1)
    * build(gradle.properties): update mod_version
    
    * feat: create 'coin' item
    
    * style: add model and texture of 'coin' item
    
    * refactor: create 'ModItems' class
    
    * refactor: create 'ModBlocks' class
    
    * refactor: create 'ModCreativeModTabs' class
    
    * refactor(ModCreativeModeTabs): customize 'Valomoney' creative mod tabs
    
    * feat: create class 'MonetaryItem' and use it for 'coin' item
    
    * feat: create 'Bill' item with model, texture and lang
    
    * style: update 'Bill' item texture
    
    * ops(github): update build.yml
    
    * test: create 'GameTests.exampleTest'
    
    * ops(github): try fixing build.yml pipeline

---
+++ src/main/java/fr/valorantage/valomoney/item/ModCreativeModeTabs.java
@@ -0,0 +1,30 @@
+package fr.valorantage.valomoney.item;
+
+import fr.valorantage.valomoney.ValomoneyMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModCreativeModeTabs {
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "valomoney" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ValomoneyMod.MODID);
+
+    // Creates a creative tab with the id "valomoney:example_tab" for the example item, that is placed after the combat tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> VALOMONEY_TAB = CREATIVE_MODE_TABS.register("valomoney_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("creativetab.valomoney.items_tab")) //The language key for the title of your CreativeModeTab
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> ModItems.COIN.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(ModItems.EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+                output.accept(ModItems.COIN.get());
+                output.accept(ModItems.BILL.get());
+            }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/fr/valorantage/valomoney/item/ModItems.java
@@ -0,0 +1,30 @@
+package fr.valorantage.valomoney.item;
+
+import fr.valorantage.valomoney.ValomoneyMod;
+import fr.valorantage.valomoney.block.ModBlocks;
+import fr.valorantage.valomoney.item.custom.MonetaryItem;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    // Create a Deferred Register to hold Items which will all be registered under the "valomoney" namespace
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ValomoneyMod.MODID);
+
+    // Creates a new BlockItem with the id "valomoney:example_block", combining the namespace and path
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", ModBlocks.EXAMPLE_BLOCK);
+    // Creates a new food item with the id "valomoney:example_id", nutrition 1 and saturation 2
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+
+    // Creates monetary items
+    public static final DeferredItem<Item> COIN = ITEMS.register("coin", () -> new MonetaryItem(1.0f));
+    public static final DeferredItem<Item> BILL = ITEMS.register("bill", () -> new MonetaryItem(5.0f));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
