6878e1ef324dcff91695eedae9147b956f91e3f9
LeLapinouAlpha <t-henon@hotmail.fr>
2025-04-29 00:12:19 +0200

    feat: add texture/models/blockstates/lang of 'ATM' block

--- src/main/java/fr/valorantage/valomoney/block/custom/ATMBlock.java
+++ src/main/java/fr/valorantage/valomoney/block/custom/ATMBlock.java
@@ -2,19 +2,37 @@ package fr.valorantage.valomoney.block.custom;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class ATMBlock extends BaseEntityBlock {
+    public static final VoxelShape SHAPE = Block.box(0, 0, 0, 15, 15, 15);
+    public static final MapCodec<ATMBlock> CODEC = simpleCodec(ATMBlock::new);
+
     public ATMBlock(Properties properties) {
         super(properties);
     }
 
     @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
     protected MapCodec<? extends BaseEntityBlock> codec() {
-        return null;
+        return CODEC;
+    }
+
+    @Override
+    protected RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 
     @Override
