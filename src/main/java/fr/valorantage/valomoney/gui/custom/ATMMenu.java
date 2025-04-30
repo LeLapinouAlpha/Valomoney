dd3df82e1183eeb67e65f8a5ff1dbeedc2e31d4d
LeLapinouAlpha <t-henon@hotmail.fr>
2025-05-01 00:29:55 +0200

    feat(ATMMenu): add player's inventory slots

--- src/main/java/fr/valorantage/valomoney/gui/custom/ATMMenu.java
+++ src/main/java/fr/valorantage/valomoney/gui/custom/ATMMenu.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -27,15 +28,32 @@ public class ATMMenu extends AbstractContainerMenu {
         this.level = inventory.player.level();
 
         this.addSlot(new SlotItemHandler(this.blockEntity.inventory, 0, 29, 24));
+        this.addPlayerInventory(inventory);
+        this.addPlayerHotbar(inventory);
     }
 
     @Override
     public ItemStack quickMoveStack(Player player, int i) {
+        // TODO
         return ItemStack.EMPTY;
     }
 
     @Override
     public boolean stillValid(Player player) {
         return stillValid(ContainerLevelAccess.create(this.level, blockEntity.getBlockPos()), player, ModBlocks.ATM.get());
+    }
+
+    private void addPlayerInventory(Inventory playerInventory) {
+        for (int i = 0; i < 3; ++i) {
+            for (int l = 0; l < 9; ++l) {
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 15 + l * 18, 86 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for (int i = 0; i < 9; ++i) {
+            this.addSlot(new Slot(playerInventory, i, 15 + i * 18, 144));
+        }
     }
 }
