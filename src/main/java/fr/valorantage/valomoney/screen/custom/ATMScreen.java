2efcc1ae9375fd61797cc891de67dee62b1d2e66
LeLapinouAlpha <t-henon@hotmail.fr>
2025-04-30 22:45:36 +0200

    style(ATMScreen): create an edit box for the money amount, and buttons to credit/debit money to/from your account

--- src/main/java/fr/valorantage/valomoney/screen/custom/ATMScreen.java
+++ src/main/java/fr/valorantage/valomoney/screen/custom/ATMScreen.java
@@ -3,6 +3,8 @@ package fr.valorantage.valomoney.screen.custom;
 import com.mojang.blaze3d.systems.RenderSystem;
 import fr.valorantage.valomoney.ValomoneyMod;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -13,6 +15,10 @@ public class ATMScreen extends AbstractContainerScreen<ATMMenu> {
     private static final ResourceLocation GUI_TEXTURE =
             ResourceLocation.fromNamespaceAndPath(ValomoneyMod.MODID, "textures/gui/atm/atm_gui.png");
 
+    private EditBox amountEditBox;
+    private Button creditButton;
+    private Button debitButton;
+
     public ATMScreen(ATMMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
 
@@ -22,13 +28,39 @@ public class ATMScreen extends AbstractContainerScreen<ATMMenu> {
     }
 
     @Override
+    protected void init() {
+        super.init();
+
+        // Create the money amount edit box
+        this.amountEditBox = new EditBox(this.font, this.leftPos + 50, this.topPos + 22, 120, 20, Component.literal("Amount"));
+        this.amountEditBox.setMaxLength(10);
+        this.amountEditBox.setVisible(true);
+        this.amountEditBox.insertText("0.00");
+        this.addRenderableWidget(this.amountEditBox);
+
+        // Create the credit button
+        this.creditButton = Button.builder(Component.literal("Credit"), button -> {})
+                .pos(this.leftPos + 50, this.topPos + 50)
+                .size(40, 20)
+                .build();
+        this.addRenderableWidget(this.creditButton);
+
+        // Create the debit button
+        this.debitButton = Button.builder(Component.literal("Debit"), button -> {})
+                .pos(this.leftPos + 105, this.topPos + 50)
+                .size(40, 20)
+                .build();
+        this.addRenderableWidget(this.debitButton);
+    }
+
+    @Override
     protected void renderBg(GuiGraphics guiGraphics, float v, int i, int i1) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, GUI_TEXTURE);
 
-        int x = (width - imageWidth) / 2;
-        int y = (height - imageHeight) / 2;
+        int x = (this.width - this.imageWidth) / 2;
+        int y = (this.height - this.imageHeight) / 2;
 
         guiGraphics.blit(GUI_TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
     }
