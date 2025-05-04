92e27bfe39ad99730046de5b18aa83f85bf141d0
LeLapinouAlpha <t-henon@hotmail.fr>
2025-05-05 01:18:15 +0200

    merge(#3): cash items trades & 'ATM' and 'Bank Card' block recipes
    * build: update github ci 'build.yml'
    
    * feat: add bank_card recipe
    
    * feat: add bank_card recipe advancement
    
    * feat: add atm recipe
    
    * feat: add atm recipe advancement
    
    * feat: add atm block loot table
    
    * feat: adding new wandering trades for cash items

---
+++ src/main/java/fr/valorantage/valomoney/event/ModEvents.java
@@ -0,0 +1,36 @@
+package fr.valorantage.valomoney.event;
+
+import fr.valorantage.valomoney.ValomoneyMod;
+import fr.valorantage.valomoney.item.ModItems;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.ItemCost;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.village.WandererTradesEvent;
+
+@EventBusSubscriber(modid = ValomoneyMod.MODID, bus = EventBusSubscriber.Bus.GAME)
+public class ModEvents {
+    @SubscribeEvent
+    public static void addWanderingTrades(WandererTradesEvent event) {
+        var genericTrades = event.getGenericTrades();
+        var rareTrades = event.getRareTrades();
+
+        genericTrades.add((trader, random) -> new MerchantOffer(
+                new ItemCost(Items.GOLD_INGOT, 1),
+                new ItemStack(ModItems.COIN.get(), 1),
+                64,
+                9,
+                0.02f
+        ));
+
+        rareTrades.add((trader, random) -> new MerchantOffer(
+                new ItemCost(Items.EMERALD, 1),
+                new ItemStack(ModItems.BILL.get(), 1),
+                32,
+                12,
+                0.02f
+        ));
+    }
+}
