a87136905dcd249bcbbd8e9adeb59d6764ba45c2
LeLapinouAlpha <t-henon@hotmail.fr>
2025-05-06 10:23:33 +0200

    feat: create 'BankCardItem'

---
+++ src/main/java/fr/valorantage/valomoney/item/custom/BankCardItem.java
@@ -0,0 +1,11 @@
+package fr.valorantage.valomoney.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class BankCardItem extends Item {
+    public BankCardItem() {
+        super(new Item.Properties());
+    }
+
+
+}
