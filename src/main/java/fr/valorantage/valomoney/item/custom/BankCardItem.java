e00ce36cb2df1770ccb29fd675515f095674ec81
LeLapinouAlpha <t-henon@hotmail.fr>
2025-05-06 14:24:51 +0200

    refactor(BankCardItem): replace 'useOn' method by 'use' method for player interaction

--- src/main/java/fr/valorantage/valomoney/item/custom/BankCardItem.java
+++ src/main/java/fr/valorantage/valomoney/item/custom/BankCardItem.java
@@ -2,16 +2,16 @@ package fr.valorantage.valomoney.item.custom;
 
 import com.mojang.logging.LogUtils;
 import fr.valorantage.valomoney.attachment.ModAttachmentTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.items.ItemStackHandler;
 import org.slf4j.Logger;
 
 import java.util.List;
@@ -30,9 +30,13 @@ public class BankCardItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
         if (!level.isClientSide()) {
-            this.player = player;
-            LOGGER.debug("{} used bank card", this.player.getDisplayName().getString());
+            LOGGER.debug("{} used bank card", player.getDisplayName().getString());
+            if (this.player == null) {
+                this.player = player;
+                LOGGER.debug("Bound bank card to player '{}'", this.player.getDisplayName().getString());
+            }
         }
+
         return super.use(level, player, usedHand);
     }
 
