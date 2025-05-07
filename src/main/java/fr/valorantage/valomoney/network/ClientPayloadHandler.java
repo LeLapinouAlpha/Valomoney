3fc6ec00ec716a784c3ca4fa9492083da77de5f7
LeLapinouAlpha <t-henon@hotmail.fr>
2025-05-07 11:02:08 +0200

    merge(#4): player's money visualization
    * feat: create 'BankCardItem'
    
    * feat: bind player to 'Bank Card' item on use and display player's balance
    
    * refactor(BankCardItem): replace 'useOn' method by 'use' method for player interaction
    
    * feat: create data component types register and register 'PLAYER_UUID' data component
    
    * feat: create a bidirectional payload 'PlayerMoneyPayload' to get player's balance in 'Bank Card' item
    
    * chore(BankCardItem): add a 'FIXME' comment

--- src/main/java/fr/valorantage/valomoney/network/ClientPayloadHandler.java
+++ src/main/java/fr/valorantage/valomoney/network/ClientPayloadHandler.java
@@ -1,7 +1,9 @@
 package fr.valorantage.valomoney.network;
 
 import com.mojang.logging.LogUtils;
+import fr.valorantage.valomoney.item.custom.BankCardItem;
 import fr.valorantage.valomoney.network.packet.ATMDebitPayload;
+import fr.valorantage.valomoney.network.packet.PlayerMoneyPayload;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.slf4j.Logger;
 
@@ -11,5 +13,12 @@ public class ClientPayloadHandler {
     public static void handleATMDebitPayloadOnNetwork(final ATMDebitPayload data, final IPayloadContext context) {
         // Do something with the data, on the network thread
         LOGGER.debug("Client received ATMDebitPayload: {}", data);
+    }
+
+    public static void handlePlayerMoneyPayloadOnNetwork(final PlayerMoneyPayload data, final IPayloadContext context) {
+        // Do something with the data, on the network thread
+        LOGGER.debug("Client received PlayerMoneyPayload: {}", data);
+
+        BankCardItem.PLAYER_MONEY = data.amount();
     }
 }
