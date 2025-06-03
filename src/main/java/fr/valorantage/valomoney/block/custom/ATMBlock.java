d8ac3f625f7406df17282a4b9a676892ad35da4f
LeLapinouAlpha <t-henon@hotmail.fr>
2025-06-03 20:57:43 +0200

    style(ATMBlock): change blockstate for placement

--- src/main/java/fr/valorantage/valomoney/block/custom/ATMBlock.java
+++ src/main/java/fr/valorantage/valomoney/block/custom/ATMBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -16,6 +17,9 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -25,6 +29,7 @@ public class ATMBlock extends BaseEntityBlock {
     // FIXME: the voxel shape must be 16x16x16 and not 15x15x15
     public static final VoxelShape SHAPE = Block.box(0, 0, 0, 15, 15, 15);
     public static final MapCodec<ATMBlock> CODEC = simpleCodec(ATMBlock::new);
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 
     public ATMBlock(Properties properties) {
         super(properties);
@@ -43,6 +48,16 @@ public class ATMBlock extends BaseEntityBlock {
     @Override
     protected RenderShape getRenderShape(BlockState state) {
         return RenderShape.MODEL;
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
