a18141331e1dbe018aacb70ba89d99aec8aa591e
LeLapinouAlpha <t-henon@hotmail.fr>
2025-06-18 13:25:56 +0200

    fix(BankCardItem): add player UUID field to 'PlayerMoneyPayload' so player see bound player balance

--- src/main/java/fr/valorantage/valomoney/item/custom/BankCardItem.java
+++ src/main/java/fr/valorantage/valomoney/item/custom/BankCardItem.java
@@ -54,8 +54,7 @@ public class BankCardItem extends Item {
             var storedPlayer = context.level().getPlayerByUUID(storedPlayerUUID);
             if (storedPlayer != null) {
                 // TODO: Optimize the number of packets send to the server (one packet sent by frame rendered)
-                // FIXME: storedPlayer is not used, so any player will see it's balance, so the data component is useless
-                PacketDistributor.sendToServer(new PlayerMoneyPayload(0));
+                PacketDistributor.sendToServer(new PlayerMoneyPayload(storedPlayerUUID, -1.f));
 
                 tooltipComponents.add(Component.literal(String.format("Money: %.2f$", PLAYER_MONEY)));
             }
