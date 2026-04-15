513628f5edcd950b0149b8ab9a444bb1a642664a
LeLapinouAlpha <t-henon@hotmail.fr>
2026-04-15 16:19:25 +0200

    style(CashItem): add custom colors to existing cash items

--- src/main/java/fr/valorantage/valomoney/item/custom/CashItem.java
+++ src/main/java/fr/valorantage/valomoney/item/custom/CashItem.java
@@ -1,21 +1,35 @@
 package fr.valorantage.valomoney.item.custom;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-
 import java.util.List;
 
 public class CashItem extends Item {
     private final float value;
+    private Style nameStyle;
 
-    public CashItem(float value) {
+    public CashItem(float value, int nameColor) {
         super(new Item.Properties());
 
         if (value <= 0.0f)
             throw new IllegalArgumentException("Monetary value must be greater than zero");
+
         this.value = value;
+        setNameColor(nameColor);
+    }
+
+    public CashItem(float value) {
+        this(value, ChatFormatting.YELLOW.getColor());
+    }
+
+    public void setNameColor(int color) {
+        this.nameStyle = Style.EMPTY.withColor(TextColor.fromRgb(color)).withBold(true);
     }
 
     public float getValue() {
@@ -23,7 +37,13 @@ public class CashItem extends Item {
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+    public Component getName(ItemStack stack) {
+        return ((MutableComponent) super.getName(stack)).withStyle(this.nameStyle);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents,
+            TooltipFlag tooltipFlag) {
         final int count = stack.getCount();
         final float value = count * this.value;
         tooltipComponents.add(Component.literal(String.format("Money: %.2f$", value)));
